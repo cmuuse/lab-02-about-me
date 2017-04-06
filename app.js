28a3a0db69d2c4d3d14009a97a49eb05282caeaf
Aydaruos <cmuuse@gmail.com>
2017-04-06 08:33:38 -0700

    added guessing game and updated list

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-
-
-
-
-
-var rain = prompt ('you think i like rain' );
-console.log('cmuuse likes rain', rain);
-if(rain == null){
-alert('please try again');
-}
-else if (rain.trim().toLowerCase() == 'yes'){
-  alert('you are from Seattle');
-
-}  else if (rain.trim().toLowerCase() == 'no'){
-  alert( 'you are from Arizona ');
-}else {
-  alert('you are from the moon')
-}
+//
+//
+//
+//
+//
+// var rain = prompt ('you think i like rain' );
+// console.log('cmuuse likes rain', rain);
+// if(rain == null){
+// alert('please try again');
+// }
+// else if (rain.trim().toLowerCase() == 'yes'){
+//   alert('you are from Seattle');
+//
+// }  else if (rain.trim().toLowerCase() == 'no'){
+//   alert( 'you are from Arizona ');
+// }else {
+//   alert('you are from the moon')
+// }
+//
+//
+// var seattle = prompt('are you from West seattle');
+//
+// console.log('you ride watertax', seattle);
+//
+// if (seattle == null){
+//    alert('you must be from Portland');
+// }else if (seattle.trim().toLowerCase() == 'yes'){
+//     alert('you  will have a long commute every week');
+//     // seattle = 'rainy city';
+// } else if (seattle.trim().toLowerCase() == 'no'){
+//   alert('you are lucky !!')
+// } else {
+//   alert('west seattle is amazing neighborhood')
+// }
+//
+//
+// var correctNumber = 15;
+// var notDone = true;
+//
+// for (var guesses = 3; guesses > 0 && notDone; guesses--){
+//
+//   var guess = prompt(' whats my fav num?');
+//   console.log('guess', guess)
+//    console.log('guesses', guesses)
+//
+//   if (guess == correctNumber) {
+//     alert('you got the right number' );
+//     notDone = false ;
+//   } else if (guesses === 1){
+//     alert('you are out of guesses')
+//   }  else {
+//     alert('guess again')
+//   }
+// }
